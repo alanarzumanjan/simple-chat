8d4dafa397e6b12da947cc6616c5b99bf8247909
martinsmb <145760580+martinsmb@users.noreply.github.com>
2024-02-29 13:17:12 +0000

    Mama Markus

--- prog4.cpp
+++ prog4.cpp
@@ -1,1 +1,24 @@
 // Martinš
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    string filename = "chat.txt";
+    string message;
+
+    ifstream infile(filename);
+    if (!infile.is_open()) {
+        cerr << "Error opening " << filename << endl;
+        return 1;
+    }
+
+    while (getline(infile, message)) {
+        cout << "Sending message to prog4: " << message << endl;
+    }
+
+    infile.close();
+
+    return 0;
+}
