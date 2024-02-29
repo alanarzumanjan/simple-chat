4988b39fd19866585407661568b47b16e0675421
martinsmb <145760580+martinsmb@users.noreply.github.com>
2024-02-29 14:10:29 +0000

    prog4

--- prog4.cpp
+++ prog4.cpp
@@ -1,5 +1,36 @@
 // Martinš
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main() {
+    string filename;
+    string message;
+
+    cout << "Choose a chat file to write to (chat.txt or chat2.txt): ";
+    cin >> filename;
+
+    ofstream outfile(filename, ios::app);
+    if (!outfile.is_open()) {
+        cerr << "Error opening " << filename << " for writing" << endl;
+        return 1;
+    }
+
+    cout << "Enter message to write to " << filename << " (type 'exit' to quit):" << endl;
+    while (true) {
+        getline(cin, message);
+        if (message == "exit")
+            break;
+        outfile << message << endl;
+    }
+
+    outfile.close();
+
+    return 0;
+}
+
+/*
 #include <iostream>
 #include <fstream>
 
@@ -26,4 +57,4 @@ int main() {
     outfile.close();
 
     return 0;
-}
+}*/
