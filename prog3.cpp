7fd643023774d159366ff1a64b757ccb968975e2
iljaprihod <147271610+iljaprihod@users.noreply.github.com>
2024-02-29 10:53:00 +0000

    Mama Markus

--- prog3.cpp
+++ prog3.cpp
@@ -1,1 +1,25 @@
-// Ilja
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    string filename = "chat.txt";
+    string message;
+
+    cout << "Enter your message: ";
+    getline(cin, message);
+
+    ofstream outfile(filename, ios::app);
+
+    if (outfile.is_open()) {
+        outfile << message << endl;
+        outfile.close();
+        cout << "Message written to " << filename << endl;
+    } else {
+        cerr << "Error opening " << filename << endl;
+        return 1;
+    }
+
+    return 0;
+}
